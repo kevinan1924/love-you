73e899528f13d376279abc3b55cf139eb90a6414
kevinan1924 <109259015+kevinan1924@users.noreply.github.com>
2024-01-09 17:37:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ function calculateLove() {
 
     // Check for the specific names "Yilun" and "Yechan"
     if ((yourName === "yilun" && crushName === "yechan") || (yourName === "yechan" && crushName === "yilun")) {
+        console.log("Congratulations! Your love is 100% true");
         document.getElementById("result-message").innerText = "Congratulations! Your love is 100% true";
     } else {
         // For other names, generate a random percentage
         percentage = Math.floor(Math.random() * 101); // 0-100
+        console.log(yourName + " and " + crushName + "'s chance of love: " + percentage + "%");
         document.getElementById("result-message").innerText = yourName + " and " + crushName + "'s chance of love:";
     }
 
